4ad780d36a5977eaa65f77a154ef9894648cdb53
AmanSingh880 <2k22.csiot.2212425@gmail.com>
2025-04-12 13:59:22 +0530

    auth

---
+++ server/controllers/authController.js
@@ -0,0 +1,42 @@
+import bcrypt from 'bcryptjs';
+import User from '../models/userModel.js';
+
+export const signup = async (req, res) => {
+  const { email, password, role } = req.body;
+
+  if (!email || !password || !role) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: 'User already exists' });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ email, password: hashedPassword, role });
+
+    await newUser.save();
+    res.status(201).json({ message: 'Signup successful' });
+
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials'});
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
+
+    res.status(200).json({ message: 'Login successful', user: { email: user.email, role: user.role } });
+
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+};
